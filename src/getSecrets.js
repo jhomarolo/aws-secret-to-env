c2d96f5adf5252c5f7c28ce96c86c097193199f5
mtperesvx <mpe@vortx.com.br>
2023-04-18 21:15:12 -0300

    refactor: refatorando arquivo bin/getSecrets para importar código core

--- src/getSecrets.js
+++ src/getSecrets.js
@@ -7,36 +7,36 @@ const config = require('./config').aws
 // Configura as credenciais da AWS
 AWS.config.update(config)
 
-  const secretsmanager = new AWS.SecretsManager()
+const secretsmanager = new AWS.SecretsManager()
 
-  async function getSecrets(secretFile) {
-    return new Promise((resolve, reject) => {
-      secretsmanager.getSecretValue({ SecretId: secretFile }, function(err, data) {
-        if (err) reject(err)
-        else resolve(data.SecretString)
-      })
+async function getSecrets(secretFile) {
+  return new Promise((resolve, reject) => {
+    secretsmanager.getSecretValue({ SecretId: secretFile }, function(err, data) {
+      if (err) reject(err)
+      else resolve(data.SecretString)
     })
-  }
+  })
+}
 
-  async function generateEnv() {
-    try {
-      const secrets = await getSecrets(process.env.SVC_NAME)
-      const jsonContent = JSON.parse(secrets)
-      const envContent = Object.keys(jsonContent).map(key => key + "=" + jsonContent[key]).join("\n")
-      fs.writeFileSync('.env', envContent)
-    } catch (error) {
-      if (error.message.includes('request is expired')){
-        console.error("⚠ Falha na autenticação: credenciais expiradas ⚠")
-      }else if (error.message.includes('request is invalid')){
-        console.error("⚠ Falha na autenticação: credenciais inválidas ⚠")
-      }else if (error.message.includes('Missing credentials in config')){
-        console.error("⚠ Falha na autenticação: credenciais não preenchidas ⚠")
-      }else if (error.message.includes("can't find the specified secret")){
-        console.error("⚠ Falha na autenticação: secret não localizada, verifique o nome do serviço. ⚠")
-      }else if (error.message.includes("'SecretId' in params")){
-        console.error("⚠ Falha na autenticação: SVC_NAME é obrigatório. ⚠")
-      }
+async function generateEnv() {
+  try {
+    const secrets = await getSecrets(process.env.SVC_NAME)
+    const jsonContent = JSON.parse(secrets)
+    const envContent = Object.keys(jsonContent).map(key => key + "=" + jsonContent[key]).join("\n")
+    fs.writeFileSync('.env', envContent)
+  } catch (error) {
+    if (error.message.includes('request is expired')){
+      console.error("⚠ Falha na autenticação: credenciais expiradas ⚠")
+    }else if (error.message.includes('request is invalid')){
+      console.error("⚠ Falha na autenticação: credenciais inválidas ⚠")
+    }else if (error.message.includes('Missing credentials in config')){
+      console.error("⚠ Falha na autenticação: credenciais não preenchidas ⚠")
+    }else if (error.message.includes("can't find the specified secret")){
+      console.error("⚠ Falha na autenticação: secret não localizada, verifique o nome do serviço. ⚠")
+    }else if (error.message.includes("'SecretId' in params")){
+      console.error("⚠ Falha na autenticação: SVC_NAME é obrigatório. ⚠")
     }
   }
+}
 
-generateEnv()
+module.exports = { generateEnv }
